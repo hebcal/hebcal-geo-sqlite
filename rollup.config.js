c3dcb657d7c4d68e5328702d19e9f0cec0b9e111
Michael J. Radwin <michael@radwin.org>
2020-06-25 13:11:56 -0700

    New module @hebcal/geo-sqlite

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+import pkg from './package.json';
+import json from '@rollup/plugin-json';
+
+export default [
+  {
+    input: 'src/index.js',
+    output: [
+      {file: pkg.main, format: 'cjs', name: pkg.name},
+    ],
+    plugins: [
+      json({compact: true}),
+      babel({
+        babelHelpers: 'bundled',
+        exclude: ['node_modules/**'],
+      }),
+      resolve(),
+      commonjs(),
+    ],
+    external: ['@hebcal/core', 'fs', 'path', 'util'],
+  },
+];
