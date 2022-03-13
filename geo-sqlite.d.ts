53a3aa9b9034b45c92253ececcb6008ca2d70e5b
Michael J. Radwin <michael@radwin.org>
2022-03-13 11:00:20 -0700

    Update types

--- geo-sqlite.d.ts
+++ geo-sqlite.d.ts
@@ -3,13 +3,25 @@
 import {Location} from '@hebcal/core';
 
 declare module '@hebcal/geo-sqlite' {
+    export type AutoComplete = {
+        id: number | string;
+        value: string;
+        geo: 'geoname' | 'zip';
+        asciiname: string;
+        admin1?: string;
+        country?: string;
+        population?: number;
+        latitude?: number;
+        longitude?: number;
+        timezone?: string;
+    };
     export class GeoDb {
         constructor(logger: any, zipsFilename: string, geonamesFilename: string);
         close(): void;
         lookupZip(zip: string): Location;
         lookupGeoname(geonameid: number): Location;
         lookupLegacyCity(cityName: string): Location;
-        autoComplete(qraw: string): Object[];
+        autoComplete(qraw: string, latlong?: boolean): AutoComplete[];
         cacheZips(): void;
         cacheGeonames(): void;
     }
